61755887adb19b70b3711d778f7260ae3af62c2b
Ruiying Yang <ruiying.yang@tuni.fi>
2020-10-05 16:02:22 +0300

    draw the login window in form editor

---
+++ Game/login.cpp
@@ -0,0 +1,14 @@
+#include "login.hh"
+#include "ui_login.h"
+
+Login::Login(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Login)
+{
+    ui->setupUi(this);
+}
+
+Login::~Login()
+{
+    delete ui;
+}
--- Game/main.cc
+++ Game/main.cc
@@ -1,9 +1,14 @@
 #include <QApplication>
-
+//#include "../../Course/CourseLib/graphics/simplemainwindow.hh"
+#include "login.hh"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Q_INIT_RESOURCE(offlinedata);
+    //Q_INIT_RESOURCE(offlinedata);
+    //CourseSide::SimpleMainWindow w;
+    //w.show();
 
+    Login logWindow;
+    logWindow.show();
     return a.exec();
 }
