f55c180cded877ba91c53d230e17e9761780e463
bshazo <haochen.zong@tuni.fi>
2020-11-24 23:26:27 +0200

    functions of judging obstacles, ground, ceiling, tile tyope and reading csv

--- Game/map/basemap.cpp
+++ Game/map/basemap.cpp
@@ -54,3 +54,81 @@ BaseMap::TileType BaseMap::getTileType(int x, int y)
     return map[y][x];
 
 }
+
+bool BaseMap::isObstacle(int x, int y)
+{
+    // if index is out of bound, then return true
+    if (x < 0 || x >= MapWidth || y < 0 || y >= MapHeight){
+        return true;
+    }
+
+    return (map[y][x] == TileType::Block);
+}
+
+bool BaseMap::isGround(int x, int y)
+{
+    // if index is out of bound, then return false
+    if (x < 0 || x >= MapWidth || y < 0 || y >= MapHeight){
+        return false;
+    }
+
+    return (map[y][x] == TileType::Block || map[y][x] == TileType::OneWayPlatform);
+
+}
+
+bool BaseMap::isCeiling(int x, int y)
+{
+    // if index is out of bound, then return false
+    if (x < 0 || x >= MapWidth || y >= MapHeight){
+        return false;
+    }
+    if (y < 0){
+        return true;
+    }
+
+    return (map[y][x] == TileType::Block || map[y][x] == TileType::OneWayPlatform);
+
+}
+
+BaseMap::TileType BaseMap::intToTileType(int tileIndex)
+{
+    switch(tileIndex){
+    case 0:
+        return TileType::Empty;
+    case 1:
+        return TileType::Block;
+    case 2:
+        return TileType::OneWayPlatform;
+    }
+    return TileType::Empty;
+}
+
+void BaseMap::readCSV(QString mapName)
+{
+    // using qt to read file instead of sstream
+    //std::ifstream input(mapFileName);
+    QFile file(mapName);
+    //if (!input.is_open())
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)){
+        std::cout << "cannot open file" << std::endl;
+        return;
+    }
+    QByteArray line;
+    std::string lineList;
+
+    for (int row = 0; row < MapHeight; ++row){
+        //std::getline(input, line);
+        line = file.readLine();
+        QString lineList = QString::fromStdString(line.toStdString());
+        QStringList stringList = lineList.split(QLatin1Char(','));
+        //std::istringstream iss(line);
+        //std::string num;
+        for (int col = 0; col < MapWidth; ++col){
+            //std::getline(iss, num, ',');
+            //std::cout << num << std::endl;
+            //map[row][col] = intToTileType(std::stoi(num));
+            map[row][col] = intToTileType(stringList.at(col).toInt());
+        }
+    }
+
+}
