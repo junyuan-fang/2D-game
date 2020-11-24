6f0ea9c133384c1dc1a2da311cdb1054b72f07fc
Ruiying Yang <ruiying.yang@tuni.fi>
2020-11-24 23:24:00 +0200

    add class sceneItem

--- Game/graphicsItem/sceneitem.hh
+++ Game/graphicsItem/sceneitem.hh
@@ -1,11 +1,26 @@
 #ifndef SCENEITEM_HH
 #define SCENEITEM_HH
 
+#include <QGraphicsPixmapItem>
+#include <QTimer>
 
-class SceneItem
+// sceneItem means the graphicsPixmapItem in the scene but this item has animation and
+// it's position will not change
+class SceneItem : public QObject, public QGraphicsPixmapItem
 {
+    Q_OBJECT
 public:
-    SceneItem();
+    explicit SceneItem(const QString itemPath, const int animationFrame, QGraphicsItem *parent = 0);
+    virtual void processCollideWithPlayer();
+    void setItemPath(const QString newItemPath, const int newAnimationFrame);
+    void playAnimation();
+
+private:
+    int frameIndex=0;
+    QString itemPath;
+    int animationFrame;
+
+
 };
 
 #endif // SCENEITEM_HH
