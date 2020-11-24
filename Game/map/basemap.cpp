c7c629c12a65546fb5f7b6703dd1f46f2104d0f4
bshazo <haochen.zong@tuni.fi>
2020-11-24 23:21:26 +0200

    completed the functions of items, player starting/end and enemy's positions

--- Game/map/basemap.cpp
+++ Game/map/basemap.cpp
@@ -1,6 +1,37 @@
 #include "basemap.hh"
 
-BaseMap::BaseMap()
+void BaseMap::initialMap(QString mapName)
 {
+    readCSV(mapName);
 
+}
+
+std::string BaseMap::getMapName()
+{
+    return "BaseMap";
+
+}
+
+QVector2D BaseMap::getGraphicsItemPosition(std::string itemName)
+{
+    if (itemName != ""){
+        return QVector2D{0,0};
+    }
+    return QVector2D{0,0};
+}
+
+QVector2D BaseMap::getPlayerStartPosition() const
+{
+    return QVector2D{0.5*penguinStandWidth,0};
+}
+
+QVector2D BaseMap::getPlayerEndPosition() const
+{
+    return QVector2D{MapWidth*TileWidth-0.5*penguinStandWidth,0};
+}
+
+QVector<QVector2D> BaseMap::getEnemyPosition() const
+{
+    QVector<QVector2D> vec;
+    return vec;
 }
