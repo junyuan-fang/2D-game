c397d511caee8e303684f0bdd825a0b16adc3534
bshazo <haochen.zong@tuni.fi>
2020-11-25 00:06:44 +0200

    buswindow functions were completed

--- Game/window/buswindow.cpp
+++ Game/window/buswindow.cpp
@@ -1,14 +1,27 @@
 #include "buswindow.hh"
 #include "ui_buswindow.h"
 
-BusWindow::BusWindow(QWidget *parent) :
+BusWindow::BusWindow(QString mapName, QWidget *parent) :
     QWidget(parent),
+    MapOfBusStop(mapName),
     ui(new Ui::BusWindow)
 {
     ui->setupUi(this);
+    initWindow();
+    connect(ui->rightButton, &QPushButton::clicked, [this](){
+        this->close();
+        emit selectedBus(ui->rightButton->text());
+    });
+
+    connect(ui->leftButton, &QPushButton::clicked, [this](){
+        this->close();
+        emit selectedBus(ui->leftButton->text());
+    });
+    connect(ui->endButton, &QPushButton::clicked, this, &QWidget::close);
 }
 
 BusWindow::~BusWindow()
 {
     delete ui;
 }
+
