3cecbecdc0c65c8a942730534592ff3b6156c263
Ruiying Yang <ruiying.yang@tuni.fi>
2020-11-24 23:56:34 +0200

    add effect when enemy is under attacked by player

--- Game/graphicsItem/hiteffect.cpp
+++ Game/graphicsItem/hiteffect.cpp
@@ -1,6 +1,32 @@
 #include "hiteffect.hh"
 
-HitEffect::HitEffect()
+#include <QDebug>
+
+HitEffect::HitEffect(const QString itemPath, const int animationFrame, QGraphicsItem *parent):
+      QGraphicsPixmapItem(parent),
+      itemPath(itemPath),
+      animationFrame(animationFrame)
 {
+    animationTimer.stop();
+    //the hitting effect should be player fast
+    animationTimer.setInterval(100);
+    connect(&animationTimer, &QTimer::timeout, this, &HitEffect::playAnimation);
+}
+
+void HitEffect::playAnimation()
+{
+    // when play once finished, it's parentItem should be invisible and stop timer
+    if (frameIndex > animationFrame){
+        animationTimer.stop();
+        frameIndex = 0;
+        this->parentItem()->setVisible(false);
+        this->setVisible(false);
+    }
+    QString pixelPath = itemPath + QString::number(frameIndex) + ".png";
+    QPixmap aniPixel(pixelPath);
+    setPixmap(aniPixel);
+    setOffset(-aniPixel.width() / 2, -aniPixel.height() / 2);
+    frameIndex += 1;
 
 }
+
