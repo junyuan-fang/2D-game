7c85872a776a5c02b7079d033814f4bdb615e81e
bshazo <haochen.zong@tuni.fi>
2020-11-24 23:35:11 +0200

    getmapname() in airportmap.cpp was done

--- Game/map/airportmap.cpp
+++ Game/map/airportmap.cpp
@@ -1,6 +1,14 @@
 #include "airportmap.hh"
 
-AirportMap::AirportMap()
+AirportsMap::AirportsMap()
 {
+    initialMap(":/mapData/mapData/airportsMap.csv");
+    // add enemy position
+    enemyPosition.append({45*TileWidth,21*TileHeight-0.5*enemyHeight});
+    enemyPosition.append({61*TileWidth, 21*TileHeight-0.5*enemyHeight});
+}
 
+std::string AirportsMap::getMapName()
+{
+    return "AirportsMap";
 }
